cfc54872502b5a04770774d4948e381685732e48
alexsmithmarist <alex.smith1@marist.edu>
2019-10-16 16:53:20 -0700

    Server updated with api calls for demo and stripe json files

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
-
+require('dotenv').config({ path: '.env' })
 
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
@@ -10,6 +7,7 @@ console.log(stripePublicKey)
 
 const express = require('express')
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
 const app = express()
 const fs = require("fs")
 const stripe = require('stripe')(stripeSecretKey)
@@ -17,6 +15,8 @@ const stripe = require('stripe')(stripeSecretKey)
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.static('Front End'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use("/static", express.static('./static/'));
 
 function getConnection(){
     return mysql.createConnection({
@@ -35,7 +35,7 @@ fs.readFile('items.json', function(error, data){
     } else{
         
         const connection = getConnection()
-        const queryString = "SELECT PartId AS id, ItemName AS name, PriceUSD as price, PartDescription as blah from parts;"
+        const queryString = "SELECT PartId AS id, ItemName AS name, PriceUSD as price, PartDescription as blah, Picture as imgName from parts;"
         
         
         connection.query(queryString, (err,result,fields) => {
@@ -45,7 +45,10 @@ fs.readFile('items.json', function(error, data){
               res.end()
               return
             }
-            
+            fs.writeFile('test.json', result, function(err){
+              if(err) throw err;
+              console.log('Saved');
+                         })
             console.log(result)
             
             res.render('shop.ejs', {
@@ -86,6 +89,37 @@ app.post('/purchase', function(req, res) {
       })
     }
   })
+})
+
+app.post('/create_listing', (req,res) => {
+    
+    const truckName = req.body.truck_name
+    const truckBrand = req.body.truck_brand
+    const truckKM = req.body.truck_km
+    const truckFuel = req.body.truck_fuel
+    const truckDrive = req.body.truck_drive
+    const truckColor = req.body.truck_color
+    
+    const email = "example2@gmail.com"
+    
+    const queryString = "insert into trucks (TruckName, Brand, KMPerHour, FuelType, DriveType, Color, EmailAddress) values (?,?,?,?,?,?,?)"
+    
+    getConnection().query(queryString, [truckName, truckBrand, truckKM, truckFuel, truckDrive, truckColor, email], (err, results, fields) => {
+        if(err) {
+            console.log("Insert failed")
+            console.log(truckName)
+            console.log(truckBrand)
+            console.log(truckKM)
+            console.log(truckFuel)
+            console.log(truckDrive)
+            console.log(truckColor)
+            console.log(email)
+            res.sendStatus(500)
+            return
+        }
+        
+        res.end()
+    })
 })
 
 app.listen(3000)
