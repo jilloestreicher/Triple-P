7ff13328584f4b9f19f3d0dfedd3c96928e06588
alexsmithmarist <alex.smith1@marist.edu>
2019-10-24 13:14:36 -0700

    Added sorting

--- routes/search.js
+++ routes/search.js
@@ -100,4 +100,112 @@ router.post('/searchTrucks', (req,res) =>{
         })
 })
 
+router.post('/sort', (req,res) =>{
+    var sortType = req.body.sortlist
+    var queryString = "SELECT PartId as id, ItemName AS name, PriceUSD as price, PartDescription as blah, Brand as brand, Picture as imgName from parts"
+    console.log(sortType)
+    if(sortType === 'name'){
+        queryString = queryString + " ORDER BY ItemName"
+    }
+    else if(sortType === 'new'){
+        queryString = queryString + " ORDER BY PartId DESC"
+    }
+    else if(sortType === 'price'){
+        queryString = queryString + " ORDER BY PriceUSD"
+    }
+    
+    getConnection().query(queryString, (err,result,fields) => {
+            if(err){
+              console.log("Failed to query: " +err)
+              res.sendStatus(500);
+              res.end()
+              return
+            }
+            fs.writeFile('test.json', result, function(err){
+              if(err) throw err;
+              console.log('Saved');
+                         })
+            console.log(result)
+            
+            res.render('shop.ejs', {
+            stripePublicKey: stripePublicKey,
+            items: result
+            })
+        })
+    
+})
+
+router.post('/sortTrucks', (req,res) =>{
+    var sortType = req.body.sortlist
+    var queryString = "SELECT TruckId AS id, TruckName AS name, EmailAddress as email, TruckDescription as blah, Picture as imgName, DriveType as drive, KMPerHour as km, FuelType as fuel, Brand as brand from trucks"
+    console.log(sortType)
+    if(sortType === 'name'){
+        queryString = queryString + " ORDER BY TruckName"
+    }
+    else if(sortType === 'new'){
+        queryString = queryString + " ORDER BY TruckId DESC"
+    }
+    else if(sortType === 'mile'){
+        queryString = queryString + " ORDER BY KMPerHour"
+    }
+    
+    getConnection().query(queryString, (err,result,fields) => {
+            if(err){
+              console.log("Failed to query: " +err)
+              res.sendStatus(500);
+              res.end()
+              return
+            }
+            fs.writeFile('test.json', result, function(err){
+              if(err) throw err;
+              console.log('Saved');
+                         })
+            console.log(result)
+            
+            res.render('trucks.ejs', {
+            stripePublicKey: stripePublicKey,
+            items: result
+            })
+        })
+    
+})
+
+router.post('/sortTrailers', (req,res) =>{
+    var sortType = req.body.sortlist
+    var queryString = "SELECT TrailerId AS id, TrailerName AS name, EmailAddress as email, TrailerDescription as blah, Picture as imgName, Length as length, Width as width, Brand as brand from trailers"
+    console.log(sortType)
+    if(sortType === 'name'){
+        queryString = queryString + " ORDER BY TrailerName"
+    }
+    else if(sortType === 'new'){
+        queryString = queryString + " ORDER BY TrailerId DESC"
+    }
+    else if(sortType === 'length'){
+        queryString = queryString + " ORDER BY Length DESC"
+    }
+    else if(sortType === 'width'){
+        queryString = queryString + " ORDER BY Width DESC"
+    }
+    
+    getConnection().query(queryString, (err,result,fields) => {
+            if(err){
+              console.log("Failed to query: " +err)
+              res.sendStatus(500);
+              res.end()
+              return
+            }
+            fs.writeFile('test.json', result, function(err){
+              if(err) throw err;
+              console.log('Saved');
+                         })
+            console.log(result)
+            
+            res.render('trailers.ejs', {
+            stripePublicKey: stripePublicKey,
+            items: result
+            })
+        })
+    
+})
+
 module.exports = router
