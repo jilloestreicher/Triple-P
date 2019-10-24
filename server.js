ad0d6389c66ee11df4f3a1ddbab26ad2caeeebd9
alexsmithmarist <alex.smith1@marist.edu>
2019-10-24 06:49:09 -0700

    Search api calls

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const stripe = require('stripe')(stripeSecretKey)
 
 app.set('view engine', 'ejs')
 app.use(express.json())
-app.use(express.static('Front End'))
+app.use(express.static('./Front End'))
 app.use(bodyParser.urlencoded({extended: false}))
-app.use("/static", express.static('./static/'));
+app.use(express.static('./static/'));
 
 function getConnection(){
     return mysql.createConnection({
@@ -27,6 +27,108 @@ function getConnection(){
     })
 }
 
+app.get('/index', function(req,res){
+    const connection = getConnection()
+    const queryString = "SELECT PartId AS id, ItemName AS name, PriceUSD as price, Picture as imgName from parts LIMIT 4;"
+    
+    connection.query(queryString, (err,result,fields) =>{
+       if(err){
+        console.log("Failed to query: " +err)
+        res.sendStatus(500);
+        res.end()
+        return
+       }
+        
+       const truckString = "SELECT TruckId AS id, TruckName as name, EmailAddress as email, TruckDescription as blah from trucks;"
+        
+       connection.query(truckString, (err,trucks,fields) =>{
+           res.render('index.ejs', {
+               items: result,
+               listings: trucks
+           })
+       })
+    })
+})
+
+app.post('/search', (req,res) =>{
+        var searchName = req.body.searchBar
+        const trimSearch = '%'+searchName+'%'
+        
+        const queryString = "SELECT PartId as id, ItemName AS name, PriceUSD as price, PartDescription as blah, Brand as brand, Picture as imgName from parts WHERE ItemName LIKE ? OR PartDescription LIKE ? OR Brand LIKE ?"
+        
+        getConnection().query(queryString, [trimSearch, trimSearch, trimSearch], (err,results,fields) =>{
+            if(err){
+              console.log("Failed to query: " +err)
+              res.sendStatus(500);
+              res.end()
+              return
+            }
+            fs.writeFile('test.json', results, function(err){
+              if(err) throw err;
+              console.log('Saved');
+            })
+            
+            
+            
+            res.render('shop.ejs', {
+              stripePublicKey: stripePublicKey,
+              items: results
+            })
+
+        })
+})
+
+app.post('/searchTrailers', (req,res) =>{
+    var searchName = req.body.searchBar
+    const trimSearch = '%'+searchName+'%'
+    
+    const queryString = "SELECT TrailerId AS id, TrailerName AS name, EmailAddress as email, TrailerDescription as blah, Picture as imgName, Length as length, Width as width, Brand as brand from trailers WHERE TrailerName LIKE ? OR TrailerDescription LIKE ? OR Brand LIKE ?;"
+    
+    getConnection().query(queryString, [trimSearch, trimSearch, trimSearch], (err,results,fields) =>{
+            if(err){
+              console.log("Failed to query: " +err)
+              res.sendStatus(500);
+              res.end()
+              return
+            }
+            fs.writeFile('test.json', results, function(err){
+              if(err) throw err;
+              console.log('Saved');
+            })
+            
+            res.render('trailers.ejs', {
+              stripePublicKey: stripePublicKey,
+              items: results
+            })
+
+        })
+})
+
+app.post('/searchTrucks', (req,res) =>{
+    var searchName = req.body.searchBar
+    const trimSearch = '%'+searchName+'%'
+    
+    const queryString = "SELECT TruckId AS id, TruckName AS name, EmailAddress as email, TruckDescription as blah, Picture as imgName, DriveType as drive, KMPerHour as km, FuelType as fuel, Brand as brand from trucks WHERE TruckName LIKE ? OR TruckDescription LIKE ? OR Brand LIKE ?;"
+    
+    getConnection().query(queryString, [trimSearch, trimSearch, trimSearch], (err,results,fields) =>{
+            if(err){
+              console.log("Failed to query: " +err)
+              res.sendStatus(500);
+              res.end()
+              return
+            }
+            fs.writeFile('test.json', results, function(err){
+              if(err) throw err;
+              console.log('Saved');
+            })
+            
+            res.render('trucks.ejs', {
+              stripePublicKey: stripePublicKey,
+              items: results
+            })
+
+        })
+})
 
 app.get('/shop', function(req,res){
 fs.readFile('items.json', function(error, data){
@@ -61,6 +163,99 @@ fs.readFile('items.json', function(error, data){
 })
 })
 
+app.get('/trucks', function(req,res){
+fs.readFile('items.json', function(error, data){
+    if(error) {
+      res.status(500).end()
+    } else{
+        
+        const connection = getConnection()
+        const queryString = "SELECT TruckId AS id, TruckName AS name, EmailAddress as email, TruckDescription as blah, Picture as imgName, DriveType as drive, KMPerHour as km, FuelType as fuel, Brand as brand from trucks;"
+        
+        
+        connection.query(queryString, (err,result,fields) => {
+            if(err){
+              console.log("Failed to query: " +err)
+              res.sendStatus(500);
+              res.end()
+              return
+            }
+            fs.writeFile('test.json', result, function(err){
+              if(err) throw err;
+              console.log('Saved');
+                         })
+            console.log(result)
+            
+            res.render('trucks.ejs', {
+            stripePublicKey: stripePublicKey,
+            items: result
+            })
+        })
+        
+    }
+})
+})
+
+app.get('/trailers', function(req,res){
+fs.readFile('items.json', function(error, data){
+    if(error) {
+      res.status(500).end()
+    } else{
+        
+        const connection = getConnection()
+        const queryString = "SELECT TrailerId AS id, TrailerName AS name, EmailAddress as email, TrailerDescription as blah, Picture as imgName, Length as length, Width as width, Brand as brand from trailers;"
+        
+        
+        connection.query(queryString, (err,result,fields) => {
+            if(err){
+              console.log("Failed to query: " +err)
+              res.sendStatus(500);
+              res.end()
+              return
+            }
+            fs.writeFile('test.json', result, function(err){
+              if(err) throw err;
+              console.log('Saved');
+                         })
+            console.log(result)
+            
+            res.render('trailers.ejs', {
+            stripePublicKey: stripePublicKey,
+            items: result
+            })
+        })
+        
+    }
+})
+})
+
+app.get('/manage-users', function(req,res){
+        
+        const connection = getConnection()
+        const queryString = "SELECT EmailAddress AS email, FirstName AS first, LastName as last, PhoneNumber as phone from accounts;"
+        
+        
+        connection.query(queryString, (err,result,fields) => {
+            if(err){
+              console.log("Failed to query: " +err)
+              res.sendStatus(500);
+              res.end()
+              return
+            }
+            fs.writeFile('test.json', result, function(err){
+              if(err) throw err;
+              console.log('Saved');
+                         })
+            console.log(result)
+            
+            res.render('manage-users.ejs', {
+            stripePublicKey: stripePublicKey,
+            items: result
+            })
+        })
+        
+})
+
 app.post('/purchase', function(req, res) {
   fs.readFile('items.json', function(error, data) {
     if (error) {
@@ -93,6 +288,8 @@ app.post('/purchase', function(req, res) {
 
 app.post('/create_listing', (req,res) => {
     
+    console.log("Creating Listing")
+    
     const truckName = req.body.truck_name
     const truckBrand = req.body.truck_brand
     const truckKM = req.body.truck_km
@@ -114,6 +311,35 @@ app.post('/create_listing', (req,res) => {
             console.log(truckDrive)
             console.log(truckColor)
             console.log(email)
+            res.sendStatus(500)
+            return
+        }
+        
+        res.end()
+    })
+})
+
+app.post('/create_part', (req,res) => {
+    
+    console.log("Creating Part")
+    
+    const partName = req.body.part_name
+    const partDesc = req.body.part_desc
+    const partPrice = req.body.part_price
+    const partBrand = req.body.part_brand
+    const partQuan = req.body.part_quan
+    
+    const queryString = "insert into parts (PartName, Brand, PriceUSD, PartDescription, QuantityOnHand) values (?,?,?,?,?)"
+    
+    getConnection().query(queryString, [partName, partBrand, partPrice, partDesc, partQuan], (err, results, fields) => {
+        if(err) {
+            console.log("Insert failed")
+            console.log(results)
+            console.log(partName)
+            console.log(partDesc)
+            console.log(partPrice)
+            console.log(partBrand)
+            console.log(partQuan)
             res.sendStatus(500)
             return
         }
