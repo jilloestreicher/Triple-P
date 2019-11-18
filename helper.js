a04ddd473e2923ee330dd3529e093f4c80905526
alexsmithmarist <alex.smith1@marist.edu>
2019-11-17 19:01:01 -0500

    Password corrected

--- helper.js
+++ helper.js
@@ -5,7 +5,7 @@ module.exports = function(){
         return mysql.createConnection({
           host:'localhost',
           user:'root',
-          password:'cyberWcap!',
+          password:'Capping2',
           database:'acsas'
         })
     }
