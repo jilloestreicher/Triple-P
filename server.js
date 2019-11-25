92af4304ceadb047385686c7aba0b6113f2a4782
herbertglaser4 <32072634+herbertglaser4@users.noreply.github.com>
2019-11-25 10:17:44 -0500

    added /checkout

--- server.js
+++ server.js
@@ -390,4 +390,16 @@ fs.readFile('items.json', function(error, data){
     }
 })
 })
+app.get('/checkout', function(req,res){
+fs.readFile('items.json', function(error, data){
+    if(error) {
+      res.status(500).end()
+    } else{
+        res.render('checkout.ejs', {
+            stripePublicKey: stripePublicKey,
+            items: JSON.parse(data)
+        })
+    }
+})
+})
 app.listen(3000)
