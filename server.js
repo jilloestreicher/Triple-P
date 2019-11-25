ef451eaa8ae503ae9d4857cfbb67b244569a16c3
katedominguez <kdoming3@gmail.com>
2019-11-25 11:12:50 -0500

    added new changeLoginButton function
    this will change the login button to log out per user session when the user is logged in.
    not complete!! need to re-pull for testing to make sure this works on all pages

--- server.js
+++ server.js
@@ -220,88 +220,6 @@ app.post('/loginCheck', [
             console.log("Successful Login");
             req.session.username = username;
 
-            //all file headers must show My Account instead of Login
-            fs.readFile('views/index.ejs', 'utf8', function (err,data) {
-                if (err) return console.log(err);
-                var result = data.replace(/login/g, 'my-account');
-                //console.log("index replaced!")
-                fs.writeFile('views/index.ejs', result, 'utf8', function (err) {
-                    if (err) return console.log(err);
-                });
-             });
-            fs.readFile('views/listing-details.ejs', 'utf8', function (err,data) {
-               if (err) return console.log(err);
-               var result = data.replace(/login/g, 'my-account');
-               //console.log("listing-details replaced!")
-               fs.writeFile('views/listing-details.ejs', result, 'utf8', function (err) {
-                   if (err) return console.log(err);
-               });
-            });
-            fs.readFile('views/manage-users.ejs', 'utf8', function (err,data) {
-               if (err) return console.log(err);
-               var result = data.replace(/login/g, 'my-account');
-               //console.log("manage-users replaced!")
-               fs.writeFile('views/manage-users.ejs', result, 'utf8', function (err) {
-                   if (err) return console.log(err);
-               });
-            });
-            fs.readFile('views/shop.ejs', 'utf8', function (err,data) {
-                if (err) return console.log(err);
-                var result = data.replace(/login/g, 'my-account');
-                //console.log("shop replaced!")
-                fs.writeFile('views/shop.ejs', result, 'utf8', function (err) {
-                    if (err) return console.log(err);
-                });
-            });
-            fs.readFile('views/shop-details.ejs', 'utf8', function (err,data) {
-                if (err) return console.log(err);
-                var result = data.replace(/login/g, 'my-account');
-                //console.log("shop-details replaced!")
-                fs.writeFile('views/shop-details.ejs', result, 'utf8', function (err) {
-                   if (err) return console.log(err);
-                });
-            });
-            fs.readFile('views/trailer-details.ejs', 'utf8', function (err,data) {
-                if (err) return console.log(err);
-                var result = data.replace(/login/g, 'my-account');
-                //console.log("trailer-details replaced!")
-                 fs.writeFile('views/trailer-details.ejs', result, 'utf8', function (err) {
-                    if (err) return console.log(err);
-                 });
-             });
-            fs.readFile('views/trailers.ejs', 'utf8', function (err,data) {
-                if (err) return console.log(err);
-                var result = data.replace(/login/g, 'my-account');
-                //console.log("trailers replaced!")
-                fs.writeFile('views/trailers.ejs', result, 'utf8', function (err) {
-                   if (err) return console.log(err);
-                });
-            });
-            fs.readFile('views/trucks.ejs', 'utf8', function (err,data) {
-                if (err) return console.log(err);
-                var result = data.replace(/login/g, 'my-account');
-                //console.log("trucks replaced!")
-                fs.writeFile('views/trucks.ejs', result, 'utf8', function (err) {
-                   if (err) return console.log(err);
-                });
-            });
-            fs.readFile('Front End/about-us.html', 'utf8', function (err,data) {
-                if (err) return console.log(err);
-                var result = data.replace(/login/g, 'my-account');
-                //console.log("about-us replaced!")
-                fs.writeFile('Front End/about-us.html', result, 'utf8', function (err) {
-                   if (err) return console.log(err);
-                });
-            });
-            fs.readFile('Front End/cart.html', 'utf8', function (err,data) {
-                if (err) return console.log(err);
-                var result = data.replace(/login/g, 'my-account');
-                //console.log("cart replaced!")
-                fs.writeFile('Front End/cart.html', result, 'utf8', function (err) {
-                   if (err) return console.log(err);
-                });
-            });
-
             //redirect user back to the home page
             const itemString = "SELECT PartId AS id, ItemName AS name, PriceUSD as price, Picture as imgName from parts LIMIT 4;"
             const truckString = "SELECT TruckId AS id, TruckName as name, EmailAddress as email, TruckDescription as blah from trucks;"
@@ -337,8 +255,8 @@ app.post('/logout', function(req, res) {
             console.log("logged out")
             fs.readFile('views/index.ejs', 'utf8', function (err,data) {
                 if (err) return console.log(err);
-                var result = data.replace(/my-account/g, 'login');
-                console.log("replaced!")
+                var result = data.replace(/Logout/g, 'login');
+                //console.log("replaced!")
                 fs.writeFile('views/index.ejs', result, 'utf8', function (err) {
                     if (err) return console.log(err);
                 });
@@ -402,4 +320,116 @@ fs.readFile('items.json', function(error, data){
     }
 })
 })
+
+app.get('/changeLoginButton', function(req,res){
+    if(req.session && req.session.username) {
+        //all file headers must show Logout instead of Login
+        fs.readFile('views/index.ejs', 'utf8', function (err,data) {
+            if (err) return console.log(err);
+            var result = data.replace(/login/g, 'Logout');
+            //console.log("index replaced!")
+            fs.writeFile('views/index.ejs', result, 'utf8', function (err) {
+                if (err) return console.log(err);
+            });
+         });
+         fs.readFile('views/index.ejs', 'utf8', function (err,data) {
+             if (err) return console.log(err);
+             var result = data.replace(/login/g, 'Logout');
+             //console.log("index replaced!")
+             fs.writeFile('views/index.ejs', result, 'utf8', function (err) {
+                 if (err) return console.log(err);
+             });
+          });
+        fs.readFile('views/listing-details.ejs', 'utf8', function (err,data) {
+           if (err) return console.log(err);
+           var result = data.replace(/login/g, 'Logout');
+           //console.log("listing-details replaced!")
+           fs.writeFile('views/listing-details.ejs', result, 'utf8', function (err) {
+               if (err) return console.log(err);
+           });
+        });
+        fs.readFile('views/manage-users.ejs', 'utf8', function (err,data) {
+           if (err) return console.log(err);
+           var result = data.replace(/login/g, 'Logout');
+           //console.log("manage-users replaced!")
+           fs.writeFile('views/manage-users.ejs', result, 'utf8', function (err) {
+               if (err) return console.log(err);
+           });
+        });
+        fs.readFile('views/shop.ejs', 'utf8', function (err,data) {
+            if (err) return console.log(err);
+            var result = data.replace(/login/g, 'Logout');
+            //console.log("shop replaced!")
+            fs.writeFile('views/shop.ejs', result, 'utf8', function (err) {
+                if (err) return console.log(err);
+            });
+        });
+        fs.readFile('views/shop-details.ejs', 'utf8', function (err,data) {
+            if (err) return console.log(err);
+            var result = data.replace(/login/g, 'Logout');
+            //console.log("shop-details replaced!")
+            fs.writeFile('views/shop-details.ejs', result, 'utf8', function (err) {
+               if (err) return console.log(err);
+            });
+        });
+        fs.readFile('views/trailer-details.ejs', 'utf8', function (err,data) {
+            if (err) return console.log(err);
+            var result = data.replace(/login/g, 'Logout');
+            //console.log("trailer-details replaced!")
+             fs.writeFile('views/trailer-details.ejs', result, 'utf8', function (err) {
+                if (err) return console.log(err);
+             });
+         });
+        fs.readFile('views/trailers.ejs', 'utf8', function (err,data) {
+            if (err) return console.log(err);
+            var result = data.replace(/login/g, 'Logout');
+            //console.log("trailers replaced!")
+            fs.writeFile('views/trailers.ejs', result, 'utf8', function (err) {
+               if (err) return console.log(err);
+            });
+        });
+        fs.readFile('views/trucks.ejs', 'utf8', function (err,data) {
+            if (err) return console.log(err);
+            var result = data.replace(/login/g, 'Logout');
+            //console.log("trucks replaced!")
+            fs.writeFile('views/trucks.ejs', result, 'utf8', function (err) {
+               if (err) return console.log(err);
+            });
+        });
+        fs.readFile('Front End/about-us.html', 'utf8', function (err,data) {
+            if (err) return console.log(err);
+            var result = data.replace(/login/g, 'Logout');
+            //console.log("about-us replaced!")
+            fs.writeFile('Front End/about-us.html', result, 'utf8', function (err) {
+               if (err) return console.log(err);
+            });
+        });
+        fs.readFile('Front End/cart.html', 'utf8', function (err,data) {
+            if (err) return console.log(err);
+            var result = data.replace(/login/g, 'Logout');
+            //console.log("cart replaced!")
+            fs.writeFile('Front End/cart.html', result, 'utf8', function (err) {
+               if (err) return console.log(err);
+            });
+        });
+    }else{
+        fs.readFile('views/index.ejs', 'utf8', function (err,data) {
+            if (err) return console.log(err);
+            var result = data.replace(/Logout/g, 'login');
+            //console.log("replaced!")
+            fs.writeFile('views/index.ejs', result, 'utf8', function (err) {
+                if (err) return console.log(err);
+            });
+        })
+        fs.readFile('views/trucks.ejs', 'utf8', function (err,data) {
+            if (err) return console.log(err);
+            var result = data.replace(/Logout/g, 'login');
+            //console.log("replaced!")
+            fs.writeFile('views/trucks.ejs', result, 'utf8', function (err) {
+                if (err) return console.log(err);
+            });
+        })
+    }
+})
+
 app.listen(3000)
