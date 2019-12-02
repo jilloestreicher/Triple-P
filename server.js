fe4bd82c7d835133ce740e6b82bc389448694980
katedominguez <kdoming3@gmail.com>
2019-12-01 21:56:19 -0500

    cleaned up login functions
    got rid of unnecessary code, used hashing verify function to make the login more efficient
    after 3 failed login attempts, it will redirect the user back to the home page

--- server.js
+++ server.js
@@ -212,46 +212,43 @@ app.post('/loginCheck', [
     //get hashed version of password
     var queryPass = "SELECT Password FROM accounts WHERE EmailAddress = ?";
 
-    helper1.getConnection().query(queryPass, [username], (err,results, field) =>{
+     helper1.getConnection().query(queryPass, [username], (err,results, field) =>{
         if(err){
           console.log("Failed to query: " +err)
-          res.redirect('/Front End/error-500.html')
+          console.log(results)
           return
-        } else{
-            var correctPass = passwordHash.verify(password, results[0].Password); //should return true or false
-
-            if(correctPass === true){
-                hashedPassword = results[0].Password;
-            }else{
-                console.log("Error -  wrong password");
-            }
-
-            var queryString = "SELECT EmailAddress, Password FROM accounts WHERE EmailAddress = ? AND Password = ?"
-
-            helper1.getConnection().query(queryString, [username, hashedPassword], (err,results, field) =>{
-                if(err){
-                  console.log("Failed to query: " +err)
-                  res.redirect('/Front End/error-500.html')
-                  return
-                } 
-
-                if(results.length == 0 || results == null){
-                    console.log("Failed Login")
-                    attempts --;
-                    if(attempts == 0){
-                        console.log("3 failed attempts");
-                        window.close();
-                    }
-                    res.redirect('/loginCheck');
-
+        }else{
+            if(results.length == 0 || results == null){
+                console.log("Failed Login")
+                attempts --;
+                if(attempts == 0){
+                    console.log("3 failed attempts");
+                    res.redirect('/index')
                 }else{
+                    res.redirect('/login.html');
+                }
+
+            }else{
+                var correctPass = passwordHash.verify(password, results[0].Password); //should return true or false
+
+                if(correctPass === true){
+                    hashedPassword = results[0].Password;
                     console.log("Successful Login");
                     req.session.username = username;
 
                     //redirect user back to the home page
-                    res.redirect('/index')
+                    res.redirect('/index');
+                }else{
+                    console.log("Error -  wrong password");
+                    attempts --;
+                    if(attempts == 0){
+                        console.log("3 failed attempts");
+                        res.redirect('/index')
+                    }else{
+                        res.redirect('/login.html');
+                    }
                 }
-            })
+            }
         }
     })
 })
@@ -273,15 +270,6 @@ app.post('/logout', function(req, res) {
             return next(err);
           }else {
             console.log("logged out")
-            fs.readFile('views/index.ejs', 'utf8', function (err,data) {
-                if (err) return console.log(err);
-                var result = data.replace(/Logout/g, 'login');
-                //console.log("replaced!")
-                fs.writeFile('views/index.ejs', result, 'utf8', function (err) {
-                    if (err) return console.log(err);
-                });
-            });
-
             res.redirect('/index')
           }
         });
@@ -377,116 +365,6 @@ fs.readFile('items.json', function(error, data){
 })
 })
 
-app.get('/changeLoginButton', function(req,res){
-    if(req.session && req.session.username) {
-        //all file headers must show Logout instead of Login
-        fs.readFile('views/index.ejs', 'utf8', function (err,data) {
-            if (err) return console.log(err);
-            var result = data.replace(/login/g, 'Logout');
-            //console.log("index replaced!")
-            fs.writeFile('views/index.ejs', result, 'utf8', function (err) {
-                if (err) return console.log(err);
-            });
-         });
-         fs.readFile('views/index.ejs', 'utf8', function (err,data) {
-             if (err) return console.log(err);
-             var result = data.replace(/login/g, 'Logout');
-             //console.log("index replaced!")
-             fs.writeFile('views/index.ejs', result, 'utf8', function (err) {
-                 if (err) return console.log(err);
-             });
-          });
-        fs.readFile('views/listing-details.ejs', 'utf8', function (err,data) {
-           if (err) return console.log(err);
-           var result = data.replace(/login/g, 'Logout');
-           //console.log("listing-details replaced!")
-           fs.writeFile('views/listing-details.ejs', result, 'utf8', function (err) {
-               if (err) return console.log(err);
-           });
-        });
-        fs.readFile('views/manage-users.ejs', 'utf8', function (err,data) {
-           if (err) return console.log(err);
-           var result = data.replace(/login/g, 'Logout');
-           //console.log("manage-users replaced!")
-           fs.writeFile('views/manage-users.ejs', result, 'utf8', function (err) {
-               if (err) return console.log(err);
-           });
-        });
-        fs.readFile('views/shop.ejs', 'utf8', function (err,data) {
-            if (err) return console.log(err);
-            var result = data.replace(/login/g, 'Logout');
-            //console.log("shop replaced!")
-            fs.writeFile('views/shop.ejs', result, 'utf8', function (err) {
-                if (err) return console.log(err);
-            });
-        });
-        fs.readFile('views/shop-details.ejs', 'utf8', function (err,data) {
-            if (err) return console.log(err);
-            var result = data.replace(/login/g, 'Logout');
-            //console.log("shop-details replaced!")
-            fs.writeFile('views/shop-details.ejs', result, 'utf8', function (err) {
-               if (err) return console.log(err);
-            });
-        });
-        fs.readFile('views/trailer-details.ejs', 'utf8', function (err,data) {
-            if (err) return console.log(err);
-            var result = data.replace(/login/g, 'Logout');
-            //console.log("trailer-details replaced!")
-             fs.writeFile('views/trailer-details.ejs', result, 'utf8', function (err) {
-                if (err) return console.log(err);
-             });
-         });
-        fs.readFile('views/trailers.ejs', 'utf8', function (err,data) {
-            if (err) return console.log(err);
-            var result = data.replace(/login/g, 'Logout');
-            //console.log("trailers replaced!")
-            fs.writeFile('views/trailers.ejs', result, 'utf8', function (err) {
-               if (err) return console.log(err);
-            });
-        });
-        fs.readFile('views/trucks.ejs', 'utf8', function (err,data) {
-            if (err) return console.log(err);
-            var result = data.replace(/login/g, 'Logout');
-            //console.log("trucks replaced!")
-            fs.writeFile('views/trucks.ejs', result, 'utf8', function (err) {
-               if (err) return console.log(err);
-            });
-        });
-        fs.readFile('Front End/about-us.html', 'utf8', function (err,data) {
-            if (err) return console.log(err);
-            var result = data.replace(/login/g, 'Logout');
-            //console.log("about-us replaced!")
-            fs.writeFile('Front End/about-us.html', result, 'utf8', function (err) {
-               if (err) return console.log(err);
-            });
-        });
-        fs.readFile('Front End/cart.html', 'utf8', function (err,data) {
-            if (err) return console.log(err);
-            var result = data.replace(/login/g, 'Logout');
-            //console.log("cart replaced!")
-            fs.writeFile('Front End/cart.html', result, 'utf8', function (err) {
-               if (err) return console.log(err);
-            });
-        });
-    }else{
-        fs.readFile('views/index.ejs', 'utf8', function (err,data) {
-            if (err) return console.log(err);
-            var result = data.replace(/Logout/g, 'login');
-            //console.log("replaced!")
-            fs.writeFile('views/index.ejs', result, 'utf8', function (err) {
-                if (err) return console.log(err);
-            });
-        })
-        fs.readFile('views/trucks.ejs', 'utf8', function (err,data) {
-            if (err) return console.log(err);
-            var result = data.replace(/Logout/g, 'login');
-            //console.log("replaced!")
-            fs.writeFile('views/trucks.ejs', result, 'utf8', function (err) {
-                if (err) return console.log(err);
-            });
-        })
-    }
-})
 
 app.get('/accountButton', function(req,res){
     //if the user is not logged in, it will direct them to the login page
